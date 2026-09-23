8f1b6feb03dbb9b8b4c3e6b656b3d1d0c8fa32cd
Deploy AVA <deploy@adegavistaalegre.com.br>
2026-09-23 13:52:24 -0300

    ddd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-
-// Para rodar build local SSR sem Vercel, use:
-// import node from '@astrojs/node';
-// adapter: node({ mode: 'standalone' }),
 
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://adegavistaalegre.com.br',
-  output: 'hybrid',
-  adapter: vercel(),
   server: {
     port: 4321,
     host: true
